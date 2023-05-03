81da3f2074cc8ec91fd7ad6523e9648d125293a3
c3d1997 <c3d1997@gmail.com>
2023-05-03 10:45:29 +0800

    kv修正 手機hover移除

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(function () {
-    $('.imgHover').hover(function () {
-        $(this).toggleClass('imgHover_after')
-        $(this).toggleClass('pHover')
-    })
-    $('.imgHover').click(function () {
-        $(this).toggleClass('imgHover_after')
-        $(this).toggleClass('pHover')
-    })
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('.imgHover').hover(function () {
+            $(this).toggleClass('imgHover_after')
+            $(this).toggleClass('pHover')
+        })
+    }
 })
